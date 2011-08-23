1fe1a5bd5b3f81ca379f17dc5cda53c33b856195
tobomedia <hou5e5>
2011-08-23 23:28:35 +0100

    basic layout and slider script

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
 /* Author:
-
+ tobomedia.com
 */
+
+
+var settings = {
+featureElem: $('.feature_wrap ul'),
+featuredContentMove: function() {
+				var featureElemPos = settings.featureElem.position();	
+				if(featureElemPos.left > -2000) {
+					console.log("move")
+				$(settings.featureElem).animate({left: + (featureElemPos.left-1000) + 'px'}, 500)
+				} else {
+				$(settings.featureElem).animate({left:'0px'}, 500)
+				}
+				setTimeout(function() {settings.featuredContentMove()}, 5000)
+				}
+}
+
+setTimeout(function() {settings.featuredContentMove()},5000)
+
+
+
 
 
 
