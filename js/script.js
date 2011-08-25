16c028afbe1dbdd6822664b6c3d04901a14b6c3c
tobomedia <hou5e5>
2011-08-25 22:25:14 +0100

    Started on footer js and general foot work

--- js/script.js
+++ js/script.js
@@ -8,16 +8,22 @@ featureElem: $('.feature_wrap ul'),
 featuredContentMove: function() {
 				var featureElemPos = settings.featureElem.position();	
 				if(featureElemPos.left > -2000) {
-					console.log("move")
-				$(settings.featureElem).animate({left: + (featureElemPos.left-1000) + 'px'}, 500)
+					$(settings.featureElem).animate({left: + (featureElemPos.left-1000) + 'px'}, 500);
 				} else {
-				$(settings.featureElem).animate({left:'0px'}, 500)
+					$(settings.featureElem).animate({left:'0px'}, 500);
 				}
-				setTimeout(function() {settings.featuredContentMove()}, 5000)
+				},
+scrollerMove: function() {
+					
+					var currentPos = $('#scroller li').position(), moveBy = currentPos.left;
+					
+					$('#scroller li').css({left:'-' + moveBy +'px'});
 				}
-}
 
-setTimeout(function() {settings.featuredContentMove()},5000)
+};
+
+setInterval(function() {settings.featuredContentMove()},5000);
+setInterval(function() {settings.scrollerMove() }, 100);
 
 
 
